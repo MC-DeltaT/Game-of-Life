d92d472790c0e1d58ef42dd69a88810017e68c06
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2019-06-02 00:02:51 +0800

    Improved benchmark info output.

--- Game of Life/main.cpp
+++ Game of Life/main.cpp
@@ -305,15 +305,18 @@ std::pair<std::size_t, std::size_t> get_console_size(HANDLE console_handle)
 
 int main()
 {
+	constexpr std::size_t num_threads = 3;
+
 #ifdef BENCHMARK
 	constexpr std::size_t rows = BENCHMARK_ROWS;
 	constexpr std::size_t cols = BENCHMARK_COLS;
 
-	std::cout << "rows: " << rows << std::endl;
-	std::cout << "columns: " << cols << std::endl;
-	std::cout << "cells: " << rows * cols << std::endl;
-	std::cout << "iterations: " << BENCHMARK_ITERATIONS << std::endl;
-	std::cout << "total cell updates: " << rows * cols * BENCHMARK_ITERATIONS << std::endl;
+	std::cout << "Rows: " << rows << std::endl;
+	std::cout << "Columns: " << cols << std::endl;
+	std::cout << "Cells: " << rows * cols << std::endl;
+	std::cout << "Iterations: " << BENCHMARK_ITERATIONS << std::endl;
+	std::cout << "Total cell updates: " << rows * cols * BENCHMARK_ITERATIONS << std::endl;
+	std::cout << "Threads: " << num_threads << std::endl;
 #else
 	HANDLE const stdout_handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	auto const [rows, cols] = get_console_size(stdout_handle);
@@ -322,8 +325,6 @@ int main()
 
 	game_grid grid(rows, cols);
 	grid.rand_init();
-
-	constexpr std::size_t num_threads = 3;
 
 	auto partitions = partition(rows * cols, num_threads);
 	std::vector<std::thread> threads;
@@ -355,12 +356,23 @@ int main()
 
 #ifdef BENCHMARK
 	auto const t2 = std::chrono::high_resolution_clock::now();
-	auto const total_time = std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1>>>(t2 - t1).count();
-	std::cout << "total time: " << total_time << " seconds" << std::endl;
-	auto const iter_per_sec = BENCHMARK_ITERATIONS / total_time;
-	std::cout << "iterations per second: " << iter_per_sec << std::endl;
-	auto const cells_per_sec = rows * cols * BENCHMARK_ITERATIONS / total_time;
-	std::cout << "cells per second: " << cells_per_sec << std::endl;
+	auto const total_real_time = std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1>>>(t2 - t1);
+	auto const real_time_per_iter = total_real_time / BENCHMARK_ITERATIONS;
+	auto const real_time_per_cell = real_time_per_iter / (rows * cols);
+	auto const total_cpu_time = total_real_time * num_threads;
+	auto const cpu_time_per_iter = total_cpu_time / BENCHMARK_ITERATIONS;
+	auto const cpu_time_per_cell = cpu_time_per_iter / (rows * cols);
+
+	std::cout << "Total real time: " << total_real_time.count() << "s" << std::endl;
+	std::cout << "Total CPU time: " << total_cpu_time.count() << "s" << std::endl;
+	std::cout << "Iterations per real second: " << 1.0 / real_time_per_iter.count() << std::endl;
+	std::cout << "Iterations per CPU second: " << 1.0 / cpu_time_per_iter.count() << std::endl;
+	std::cout << "Cell updates per real second: " << 1.0 / real_time_per_cell.count() << std::endl;
+	std::cout << "Cell updates per CPU second: " << 1.0 / cpu_time_per_cell.count() << std::endl;
+	std::cout << "Real time per cell update: " <<
+		std::chrono::duration_cast<std::chrono::duration<double, std::nano>>(real_time_per_cell).count() << "ns" << std::endl;
+	std::cout << "CPU time per cell update: " <<
+		std::chrono::duration_cast<std::chrono::duration<double, std::nano>>(cpu_time_per_cell).count() << "ns" << std::endl;
 #endif
 
 	exit = true;
